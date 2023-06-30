64683f002f08778157ed259a15d9f281fe6aa154
Miguel Martins <miguelrjmartins9@gmail.com>
2023-06-30 11:29:02 -0400

    feat: create theme

---
+++ src/theme/index.ts
@@ -0,0 +1,16 @@
+export default {
+  COLORS: {
+    BACKGROUND: '#ffffff',
+    PRIMARY: '#ffd6e4',
+    ALERT: '#ff0000',
+    DISABLED: '#cccccc',
+    INPUT: '#eeeeee',
+    TEXT: '#202020',
+    TEXT_LIGHT: '#303030',
+  },
+  FONTS: {
+    REGULAR: 'Lato_400Regular',
+    BOLD: 'Lato_700Bold',
+    BLACK: 'Lato_900Black'
+  }
+};
